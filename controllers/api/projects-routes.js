9749517dac85e91b7882e3fa646649daa3982ead
RaiderNationBuilder <71417297+RaiderNationBuilder@users.noreply.github.com>
2021-12-04 17:45:26 -0600

    update

--- controllers/api/projects-routes.js
+++ controllers/api/projects-routes.js
@@ -1,16 +1,16 @@
 const router = require('express').Router();
-const {Projects, Cards, Columns, Users} = require('../../model');
+const { Projects, Cards, Columns, Users } = require('../../model');
 const sequelize = require('../../config/connection');
 const withAuth = require('../../utils/auth');
 
 // TJ's test data
-const projects = require('../../test-data');
+// const projects = require('../../test-data');
 
 // model: projects: name
 
 router.get('/projects/:id', (req, res) => {
   const id = req.params.id;
-  res.render('project', {projects});
+  res.render('project', { projects });
 });
 
 // post create a new project
@@ -25,5 +25,23 @@ router.post('/projects', (req, res) => {
     })
   );
 });
+
+// Delete a project
+router.delete('/projects/:projectId', (req, res) => {
+  Projects.destroy({
+    id: req.body.projectId,
+  },
+    {
+      where: {
+        id: req.body.projectId,
+      },
+    }
+  ).then(dbPostData =>
+    res.json(dbPostData).catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
+  );
+})
 
 module.exports = router;

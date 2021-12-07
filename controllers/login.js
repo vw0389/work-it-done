e27ed7ed4e1a2cfda62022730548cfe04d2f70cc
Victor Weinert <dev@vweinert.com>
2021-12-07 16:22:25 -0600

    added register route

--- controllers/login.js
+++ controllers/login.js
@@ -22,10 +22,41 @@ router.post('/login', (req, res) => {
             req.session.user_id = User.id;
             req.session.email = User.email;
             req.session.loggedIn = true;
+
+            delete User.dataValues.password;
+
+            res.json({ user: User, message: 'Logged in' });
         });
     });
 });
+router.post('/register', (req, res) => {
+    Users.findOne({
+        where: {
+            email: req.body.email,
+        },
+    }).then(User => {
+        if (!User) {
+            Users.create({
+                email: req.body.email,
+                password: req.body.password
+            }).then(() => {
+                req.session.save(() => {
+                    // declare session variables
+                    req.session.user_id = User.id;
+                    req.session.email = User.email;
+                    req.session.loggedIn = true;
+                    delete User.dataValues.password;
 
+                    res.json({ user: User, message: 'Logged in' });
+                });
+            })
+        } else {
+            res.status(400).json({ message: "that user already exists" });
+        }
+
+
+    });
+});
 router.post('/logout', (req, res) => {
     if (req.session.loggedIn) {
         req.session.destroy(() => {

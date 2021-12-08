599b734ca116e979c3357a3e7cf22c55f00fa35e
TJ James <jamestw13@gmail.com>
2021-12-07 20:26:02 -0600

    refactored column and card js files

---
+++ Public/javascript/project.js
@@ -0,0 +1,70 @@
+// Create new Project
+// Button activation
+$('#new-project-button').on('click', () => {
+  const textInput = $('<input>')
+    .addClass('project-tab ui-tabs-anchor')
+    .attr('id', 'new-project-name')
+    .attr('placeholder', 'Project Name');
+
+  $('#new-project-button').replaceWith(textInput);
+  $('#new-project-name').trigger('focus');
+});
+
+// Enter name and send to database
+$('#project-list').on('blur', 'input', async () => {
+  const projectName = $('#new-project-name').val().trim();
+
+  if (!projectName) {
+    document.location.reload();
+  } else {
+    const response = await fetch('/api/projects', {
+      method: 'POST',
+      body: JSON.stringify({
+        name: projectName,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      console.log(response);
+    }
+  }
+});
+
+// Edit Project Name
+$('.project-name').on('click', 'h3', function () {
+  const projectName = $(this).text().trim();
+  const textInput = $('<input>').attr('type', 'text').addClass('project-name').val(projectName);
+  $(this).replaceWith(textInput);
+  textInput.trigger('focus');
+});
+
+$('.project-name').on('blur', 'input', async function () {
+  const projectName = $(this).val().trim();
+  const projectId = $(this).closest('.project-workspace').attr('id').replace('project-', '');
+
+  const response = await fetch(`/api/projects/${projectId}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      projectId,
+      projectName,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (response.ok) {
+    document.location.reload();
+  } else {
+    console.log(response.statusText);
+  }
+
+  const nameElement = $('<h3>').text(projectName);
+
+  $(this).replaceWith(nameElement);
+});

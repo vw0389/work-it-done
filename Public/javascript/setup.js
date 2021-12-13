d0136ca11bc732cfc53c7af3a905365f21bd012e
TJ James <jamestw13@gmail.com>
2021-12-13 17:00:44 -0600

    updated jqueryui functions for dragging/dropping

--- Public/javascript/setup.js
+++ Public/javascript/setup.js
@@ -1,17 +1,21 @@
+//////// SETTING UP JQUERY UI ELEMENTS ////////
+
+// Project tabs are draggable for deletion purposes
 $('.project-tab').draggable({
   items: 'li:not(.not-sortable)',
   revert: 'invalid',
 });
 
+// Tab selection functionality - clicking tab opens the project view
 $('#tabs')
   .tabs({collapsible: true, active: false, heightStyle: 'content'})
   .addClass('ui-tabs-vertical ui-helper-clearfix');
 $('#tabs li').removeClass('ui-corner-top').addClass('ui-corner-left');
 
-// // Make columns sortable
-$('.column-wrapper').draggable({revert: 'invalid', containment: 'window'});
+// Columns are draggable for deletion purposes
+$('.column-wrapper').draggable({revert: 'invalid', helper: 'clone', appendTo: document.body, cursorAt: {left: 5}});
 
-// Make cards sortable
+// Cards are sortable
 $('.column').sortable({
   receive: updateCardColumn,
   connectWith: '.column',
@@ -20,18 +24,15 @@ $('.column').sortable({
 });
 // .disableSelection();
 
-// Make cards toggleable
+// Cards are collapsable
 $('.card-toggle').accordion({collapsible: true, active: false, heightStyle: 'content'});
 
-$('#login-button').on('click', () => {
-  window.location.replace('/workspace');
-});
-
+// Trash element is droppable - received project tabs, columns, and cards
 $('#trash').droppable({
   accept: '.column-wrapper .card .project-tab1',
   tolerance: 'touch',
   drop: () => {
-    // Delete functionality
+    // TODO: Delete functionality
   },
   over: () => {
     $('#trash').addClass('trash-active');
